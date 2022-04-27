d8b8104593caa940baa3bb528d4ba8260676362a
Pawel Kowalewski <pkowalewski@microstrategy.com>
2022-04-27 17:19:28 +0200

    variables added

--- contracts/ChainlinkRandomNumber.sol
+++ contracts/ChainlinkRandomNumber.sol
@@ -5,5 +5,11 @@ import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
 contract VRFD20 is VRFConsumerBaseV2 {
-
+    uint64 s_subscriptionId;
+    address vrfCoordinator = 0x6168499c0cFfCaCD319c818142124B7A15E857ab;
+    bytes32 s_keyHash =
+        0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
+    uint32 callbackGasLimit = 40000;
+    uint16 requestConfirmations = 3;
+    uint32 numWords = 1;
 }
