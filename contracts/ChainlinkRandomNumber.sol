96fac2979f5b69516fcb0ec7c6e17910d8c33be3
Pawel Kowalewski <pkowalewski@microstrategy.com>
2022-04-28 15:53:01 +0200

    RandomNumber contract added

--- contracts/ChainlinkRandomNumber.sol
+++ contracts/ChainlinkRandomNumber.sol
@@ -3,8 +3,13 @@ pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract VRFD20 is VRFConsumerBaseV2 {
+    VRFCoordinatorV2Interface COORDINATOR;
+
+    address s_owner;
+
     uint64 s_subscriptionId;
     address vrfCoordinator = 0x6168499c0cFfCaCD319c818142124B7A15E857ab;
     bytes32 s_keyHash =
@@ -12,4 +17,86 @@ contract VRFD20 is VRFConsumerBaseV2 {
     uint32 callbackGasLimit = 40000;
     uint16 requestConfirmations = 3;
     uint32 numWords = 1;
+    uint256 private constant ROLL_IN_PROGRESS = 42;
+
+    mapping(uint256 => address) private s_rollers;
+    mapping(address => uint256) private s_results;
+
+    event DiceRolled(uint256 indexed requestId, address indexed roller);
+    event DiceLanded(uint256 indexed requestId, uint256 indexed result);
+
+    constructor(uint64 subscriptionId) VRFConsumerBaseV2(vrfCoordinator) {
+        COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
+        s_owner = msg.sender;
+        s_subscriptionId = subscriptionId;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == s_owner);
+        _;
+    }
+
+    function rollDice(address roller)
+        public
+        onlyOwner
+        returns (uint256 requestId)
+    {
+        require(s_results[roller] == 0, "Already rolled");
+        requestId = COORDINATOR.requestRandomWords(
+            s_keyHash,
+            s_subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            numWords
+        );
+
+        s_rollers[requestId] = roller;
+        s_results[roller] = ROLL_IN_PROGRESS;
+        emit DiceRolled(requestId, roller);
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
+        internal
+        override
+    {
+        uint256 d20Value = (randomWords[0] % 20) + 1;
+        s_results[s_rollers[requestId]] = d20Value;
+        emit DiceLanded(requestId, d20Value);
+    }
+
+    function house(address player) public view returns (string memory) {
+        require(s_results[player] != 0, "Dice not rolled");
+        require(s_results[player] != ROLL_IN_PROGRESS, "Roll in progress");
+        return getHouseName(s_results[player]);
+    }
+
+    // getHouseName function
+    function getHouseName(uint256 id) private pure returns (string memory) {
+        // array storing the list of house's names
+        string[20] memory houseNames = [
+            "Targaryen",
+            "Lannister",
+            "Stark",
+            "Tyrell",
+            "Baratheon",
+            "Martell",
+            "Tully",
+            "Bolton",
+            "Greyjoy",
+            "Arryn",
+            "Frey",
+            "Mormont",
+            "Tarley",
+            "Dayne",
+            "Umber",
+            "Valeryon",
+            "Manderly",
+            "Clegane",
+            "Glover",
+            "Karstark"
+        ];
+
+        // returns the house name given an index
+        return houseNames[id - 1];
+    }
 }
